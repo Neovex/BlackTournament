3c298ab6e4b033bc195d5306cae220ea2360cc6b
Neovex <alexander.schwahl@web.de>
2016-03-07 18:06:34 +0000

    INITIAL COMMIT

---
+++ BlackTournament/Game.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BlackCoat;
+
+namespace BlackTournament
+{
+    public class Game
+    {
+        public static void Main(string[] args)
+        {
+            var core = new Core(Core.DefaultDevice, true);
+            core.Run();
+        }
+    }
+}
