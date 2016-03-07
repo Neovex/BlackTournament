f22c1d1401491e96afe39afc4c2128afc27e6d48
Neovex <alexander.schwahl@web.de>
2016-03-07 20:29:20 +0000

    Added TMX parsing module Added basic tile renderer

--- BlackTournament/Game.cs
+++ BlackTournament/Game.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using BlackCoat;
+using TiledSharp;
+using SFML.System;
+using SFML.Graphics;
+using SFML.Window;
 
 namespace BlackTournament
 {
@@ -11,6 +15,33 @@ namespace BlackTournament
         public static void Main(string[] args)
         {
             var core = new Core(Core.DefaultDevice, true);
+            var map = new Map(core, "Maps\\SpaceArena.tmx");
+            map.Load();
+            core.Layer_Game.AddChild(map);
+
+            map.View = new SFML.Graphics.View(new FloatRect(0, 0, core.DefaultView.Size.X / 2, core.DefaultView.Size.Y / 2));
+
+            core.OnUpdate += deltaT =>
+            {
+                var speed = 400;
+                if (Input.IsKeyDown(Keyboard.Key.Left))
+                {
+                    map.View.Center += new Vector2f(speed * -deltaT, 0);
+                }
+                if (Input.IsKeyDown(Keyboard.Key.Right))
+                {
+                    map.View.Center += new Vector2f(speed * deltaT, 0);
+                }
+                if (Input.IsKeyDown(Keyboard.Key.Up))
+                {
+                    map.View.Center += new Vector2f(0, speed * -deltaT);
+                }
+                if (Input.IsKeyDown(Keyboard.Key.Down))
+                {
+                    map.View.Center += new Vector2f(0, speed * deltaT);
+                }
+            };
+
             core.Run();
         }
     }
---
+++ BlackTournament/Map.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BlackCoat.Entities;
+using TiledSharp;
+using BlackCoat;
+using System.IO;
+using SFML.Graphics;
+using SFML.System;
+
+namespace BlackTournament
+{
+    public class Map : Container
+    {
+        private String _Path;
+        private TmxMap _MapData;
+        private Dictionary<TmxTileset, Texture> _TileTextures;
+
+        public Map(Core core,String path):base(core)
+        {
+            if (!File.Exists(path)) throw new FileNotFoundException(path);
+            _Path = path;
+        }
+
+        public bool Load()
+        {
+            //try
+            {
+                _MapData = new TmxMap(_Path);
+                _Core.ClearColor = new Color((byte)_MapData.BackgroundColor.R, (byte)_MapData.BackgroundColor.G, (byte)_MapData.BackgroundColor.B);
+                _TileTextures = _MapData.Tilesets.ToDictionary(ts=>ts, ts => new Texture(ts.Image.Source));
+                
+                foreach (var layer in _MapData.Layers)
+                {
+                    var c = new Container(_Core);
+                    c.Position = new Vector2f((float)(layer.OffsetX ?? 0), (float)(layer.OffsetY ?? 0));
+                    foreach (var tile in layer.Tiles)
+                    {
+                        if (tile.Gid == 0) continue;
+                        var set = _TileTextures.Where(kvp=>kvp.Key.FirstGid<=tile.Gid).Max();
+                        var g = new Graphic(_Core);
+                        g.Texture = set.Value;
+                        g.Position = new Vector2f(tile.X * _MapData.TileWidth, tile.Y * _MapData.TileHeight);
+                        g.TextureRect = new IntRect(((tile.Gid - 1) % set.Key.Columns.Value) * _MapData.TileWidth, ((tile.Gid-1) / set.Key.Columns.Value) * _MapData.TileHeight, _MapData.TileWidth, _MapData.TileHeight);
+                        c.AddChild(g);
+                    }
+                    AddChild(c);
+                }
+            }
+            //catch (Exception e)
+            {
+                //_Core.Log(e);
+            }
+            return true;
+        }
+
+
+    }
+}
